9a81fa94544a7abb4f8a900995f8ae3c0eb98dba
imenbm <imen.benmhd@gmail.com>
2022-03-11 15:24:56 +0100

    added splitting function

--- database.py
+++ database.py
@@ -2,11 +2,14 @@
 
 import numpy as np
 import csv
+from sklearn import model_selection
+
+
+
+
 data_base = ['winequality-white.csv',
         'winequality-red.csv',
-        'housing.data'
-
-        ]
+        'housing.data' ]
 
 
 def load(data):
@@ -15,7 +18,7 @@ def load(data):
       return np.loadtxt("Data/housing.data")  
 
   else:  
-    with open('Data/'+ data +'.csv', 'rt') as f:
+    with open('Data/'+ data_base[data], 'rt') as f:
         reader = csv.reader(f,delimiter=';')
         data_=[]
         next(reader)
@@ -26,6 +29,17 @@ def load(data):
 
 
 def split_data(data,seed):
+    
+    X=data[:,:-1]
+    Y=data[:,-1]
+    print(Y)
+    x_train ,x_test,y_train , y_test = model_selection.train_test_split(X,Y,test_size = 0.5, random_state =seed)
+    return x_train,x_test,y_train,y_test 
+
+
+
+
+
 
 
 
