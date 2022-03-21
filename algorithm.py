2210259ba86f05ea673e5bdb6f318b52b3b33259
imenbm <imen.benmhd@gmail.com>
2022-03-21 13:29:53 +0100

    fix some issues

--- algorithm.py
+++ algorithm.py
@@ -4,7 +4,7 @@ import preprocessor
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 
-normalization=['PolynomialFeaturesScaler', 'MinMaxScaler','StandardScaler','normalize']
+normalization=['MinMaxScaler_','StandardScaler','PolynomialFeaturesScaler']
 
 
 def regression(data,norm,model):
@@ -12,22 +12,39 @@ def regression(data,norm,model):
     y_tested=[]
 
     for i in database.seeds:
-        training_set=database.extract(data,i,"train")
-        testing_set=database.extract(data,i,"test")
-        normalization_to_call=getattr(database , normalization[norm])
+        training_set=database.extract(data,i,0)
+        testing_set=database.extract(data,i,1)
+        degree=training_set.shape[1]-1
+        if norm==2:
+            print("norma")
+            normalization_to_call=getattr(preprocessor , normalization[norm])
+            print(training_set.shape[1])
+            normalized_train_set=normalization_to_call(training_set,"minmax", degree)
+            normalized_test_set=normalization_to_call(testing_set,"minmax",degree)
+            print("oknor")
+        if norm==3:
+            normalization_to_call=getattr(preprocessor , normalization[norm])
 
-        
-        normalized_train_set=normalization_to_call(training_set)
+            normalized_train_set=normalization_to_call(training_set,"z-norm",degree)
+            normalized_test_set=normalization_to_call(testing_set,"z-norm",degree)
+        else:
+            normalization_to_call=getattr(preprocessor , normalization[norm])
+
+            normalized_train_set=normalization_to_call(training_set)
+            normalized_test_set=normalization_to_call(testing_set)
+
         y_train=normalized_train_set[:,-1]
-        normalized_test_set=normalization_to_call(testing_set)
         y_test=normalized_test_set[:,-1]
+
         if model=="LinearRegression":
+            print("model")
             regressor = LinearRegression()
         if model=="Regressiontree":
             regressor=DecisionTreeRegressor()
 
         regressor.fit(normalized_train_set,y_train)
         y_predict=regressor.predict(normalized_test_set)
+        print(y_predict)
         y_tested.append(y_test)
         y_predicted.append(y_predict)
 
@@ -35,3 +52,4 @@ def regression(data,norm,model):
 
 
 
+ytest, ypredict=regression(0,0,"LinearRegression")
