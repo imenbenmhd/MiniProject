1dffb058e508f3693a685095da1bad9040ee1fce
imenbm <imen.benmhd@gmail.com>
2022-03-25 16:04:58 +0100

    create unit test database

---
+++ test-database.py
@@ -0,0 +1,17 @@
+
+import algorithm
+import database
+import preprocessor
+import analysis
+
+
+def test_load_1():
+    i_1=0
+    i_2=1
+    r_1=database.load(i_1)
+    r_2=database.load(i_2)
+    assert(r_1.shape[1]==r_2.shape[1]==12)
+
+
+
+
