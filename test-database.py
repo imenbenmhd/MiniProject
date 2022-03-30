1d6c8fa495405a3f36d4c8ed8a5b2eecb11f8149
imenbm <imen.benmhd@gmail.com>
2022-03-30 10:48:00 +0200

    add split testing

--- test-database.py
+++ test-database.py
@@ -1,9 +1,37 @@
 import database
+import pytest
 
 
 def test_load_1():
     i_1=0
     i_2=1
+    i_3=2
     r_1=database.load(i_1)
     r_2=database.load(i_2)
+    r_3=database.load(i_3)
     assert(r_1.shape[1]==r_2.shape[1]==12)
+    assert(r_1.shape[1] != r_3.shape[1])
+
+
+def test_split_1():
+    r_1=database.split_data(0)
+    assert(list(r_1)==database.seeds)
+    for i in r_1.keys():
+
+        assert(list(r_1[i])==database.subsets)
+
+def test_split_2():
+    for j in range(3):
+        r_1=database.split_data(j) 
+
+        for i in r_1.keys():
+            assert(r_1[i]["train"].shape[0]==pytest.approx(r_1[i]["test"].shape[0],1))
+
+
+
+
+
+
+
+
+
