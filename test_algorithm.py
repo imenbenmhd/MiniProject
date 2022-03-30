72c863dd11b434814eee8e686af2f09deab5295d
imenbm <imen.benmhd@gmail.com>
2022-03-30 10:46:59 +0200

    unittesting algorithm

---
+++ test_algorithm.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+import algorithm
+
+
+
+def test_regression_1():
+    for i in range(2):
+        for j in range(3):
+            ytest,ypred=algorithm.regression(i,j,"LinearRegression")
+            assert(len(ytest[0])==len(ypred[0]))
+            ytest,ypred=algorithm.regression(i,j,"Regressiontree")
+            assert(len(ytest[0])==len(ypred[0]))
+
+
+    
+
+
