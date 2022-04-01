e93e33dc26d1591a854f932114215a4c489a0e3b
Teodora <teca.glamocanin@gmail.com>
2022-04-01 23:30:47 +0200

    Added MAE function

--- analysis.py
+++ analysis.py
@@ -1,1 +1,23 @@
-Test
+from sklearn.metrics import mean_absolute_error
+import database
+import algorithm
+import numpy as np
+
+def mean_abs_err(prediction, true_labels):
+    """
+    Calculates the mean absolute error
+    Parameters
+    ==========
+    prediction : numpy.ndarray
+        A 1D :py:class:`numpy.ndarray` containing your prediction
+    true_labels : numpy.ndarray
+          A 1D :py:class:`numpy.ndarray` containing the ground truth labels for
+          the input array, organized in the same order.
+    Returns
+    =======
+    MAE : float
+        The classification error rate
+    """
+
+    mae = (np.abs(prediction - true_labels)).sum() / len(prediction)
+    return mae
