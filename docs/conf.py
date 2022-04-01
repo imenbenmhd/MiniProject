8df660dc690931431ba9bc58e0f7eefd9d49ac83
imenbm <imen.benmhd@gmail.com>
2022-04-01 11:18:12 +0200

    change theme

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,8 @@ author = 'Imen Ben Mahmoud and Theodora glamocanin'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_rtd_theme
+
 extensions = [ "sphinx.ext.todo",
     "sphinx.ext.coverage",
     "sphinx.ext.ifconfig",
@@ -80,3 +82,14 @@ source_suffix = ".rst"
 
 # The master toctree document.
 master_doc = "index"
+
+pygments_style = "sphinx"
+html_theme = "sphinx_rtd_theme"
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+# html_theme_options = {}
+
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
