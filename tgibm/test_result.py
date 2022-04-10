3da0281bb21d2aba8e51e996b849dba94ecdb506
Teodora <teca.glamocanin@gmail.com>
2022-04-10 15:46:35 +0200

    add test for result

---
+++ tgibm/test_result.py
@@ -0,0 +1,36 @@
+"""Test unit for result code"""
+
+from . import result
+import numpy as np
+
+def mae_size(data, norm, expected):
+    """Runs a single test case for size of MAE table
+    Parameters
+    ==========
+    data : int
+        Dataset to analyze
+    norm : int
+        Normalization method
+    expected : tuple
+        The expected dimension of table
+    Raises
+    ======
+    AssertionError
+        In case something goes wrong
+    """
+
+    data = [0, 1, 2]
+    norm = [0, 1, 2, 3]
+
+    mae = result.test_mae(data, norm)[0].shape
+
+    assert np.isclose(mae, expected), "Expected %r, but got %r" % (
+        expected,
+        mae,
+    )
+
+def test_norms_01():
+    mae_size(0, 1, (3, 2))
+
+def test_norms_12():
+    mae_size(1, 2, (3, 2))
